b8f75528314555fb263e7e6900237c59700cccb0
Ivan Deniskin <ivanmordwin@yandex.ru>
2018-03-27 18:52:00 +0300

    Project structure

---
+++ sources/kelvin-node.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv) {
+    printf("Kelvin Node version 0.1");
+    sleep(5);
+    return 0;
+}
