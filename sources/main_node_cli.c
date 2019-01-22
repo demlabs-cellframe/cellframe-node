85e877dd710597ecf35342e767cbebc39530c2fa
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2019-01-22 20:28:41 +0700

    [*] Massive submodules update [+] Testnet root public keys [+] New kelvin-node-tool and kelvin-node-cli for kelvin node operations

---
+++ sources/main_node_cli.c
@@ -0,0 +1,30 @@
+/*
+ * Authors:
+ * Dmitriy A. Gearasimov <kahovski@gmail.com>
+ * DeM Labs Inc.   https://demlabs.net
+ * DeM Labs Open source community https://github.com/demlabsinc
+ * Copyright  (c) 2017-2019
+ * All rights reserved.
+
+ This file is part of DAP (Deus Applications Prototypes) the open source project
+
+    DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DAP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
