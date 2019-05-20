31b2865b36c7d8a41d18df601cf882e7c2716a34
Aleksandr Lysikov <lysikov@inbox.ru>
2019-05-20 22:38:12 +0500

    added libdap-chain-gdb added support ledger update modules

--- sources/main.c
+++ sources/main.c
@@ -43,6 +43,7 @@
 #include "dap_chain_cs_dag.h"
 #include "dap_chain_cs_dag_poa.h"
 #include "dap_chain_cs_dag_pos.h"
+#include "dap_chain_gdb.h"
 
 #include "dap_chain_net.h"
 #include "dap_chain_net_srv.h"
@@ -171,7 +172,6 @@ int main(int argc, const char * argv[])
         return -61;
     }
 
-
     if( dap_chain_cs_dag_init() !=0){
         log_it(L_CRITICAL,"Can't init dap chain dag consensus module");
         return -62;
@@ -185,6 +185,11 @@ int main(int argc, const char * argv[])
     if( dap_chain_cs_dag_pos_init() !=0){
         log_it(L_CRITICAL,"Can't init dap chain dag consensus PoA module");
         return -64;
+    }
+
+    if(dap_chain_gdb_init() != 0) {
+        log_it(L_CRITICAL, "Can't init dap chain gdb module");
+        return -71;
     }
 
     if( dap_chain_net_init() !=0){
