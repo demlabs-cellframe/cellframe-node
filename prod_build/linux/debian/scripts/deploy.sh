1cf9e6fff48c94abe1c5cd923cc13a53acd58662
alexandr.kravchenko <alexander.kravchenko@demlabs.net>
2020-02-13 16:30:33 +0500

    Working on deployment to debian.pub.demlabs.net

--- prod_build/linux/debian/scripts/deploy.sh
+++ prod_build/linux/debian/scripts/deploy.sh
@@ -3,21 +3,26 @@
 echo "Deploying to $PACKAGE_PATH"
 echo $wd
 cd $REPO_DIR
-PKGFILES=($(ls build/*.deb))
-mv build/*.deb $wd/$PACKAGE_PATH || echo "[ERR] Something went wrong in publishing the package. Now aborting."
+cd build
+PKGFILES=($(ls *.deb))
+cd ..
+mv build/*.deb $wd/$PACKAGE_PATH/ || echo "[ERR] Something went wrong in publishing the package. Now aborting."
 
-if [ ! -z $UPDVER ]; then
+NOTONBUILDSERVER=0
+gitlab-runner -v 2&>> /dev/null || NOTONBUILDSERVER=$?
+if [[ $NOTONBUILDSERVER == 0 ]]; then
+	mkdir -p $REPO_DIR_SRC
 	for pkgfile in $PKGFILES; do
-		ln -sf $wd/$PACKAGE_PATH/$pkgfile $wd/$PACKAGE_PATH/$pkgfile-latest
-		cp $wd/$PACKAGE_PATH/$pkgfile $REPO_DIR_SRC
+		cp -v $wd/$PACKAGE_PATH/$pkgfile $REPO_DIR_SRC
 	done
-	sudo reprepro -c "$DISTR_COMPONENT" --ask-passphrase includedeb "$DISTR_CODENAME" $REPO_DIR_SRC/*.deb
+	cd $REPO_DIR
+	sudo reprepro -C "$DISTR_COMPONENT" --ask-passphrase includedeb "$DISTR_CODENAME" $REPO_DIR_SRC/*.deb
 	#Update into reprepro:
 	
-	export -n "UPDVER"
 	#Need to create latest symlink to the project.
 fi
-
+	export -n "UPDVER"
+rm -r $REPO_DIR_SRC
 cd $wd
 #symlink name-actual to the latest version.
 #build/deb/versions - for all files
