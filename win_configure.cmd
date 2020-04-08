11cf40bf6eb39ee5714179205da22399e352bd0f
Constantin Papizh <p.const@bk.ru>
2020-04-08 14:58:22 +0300

    Linking errors fixed

--- win_configure.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set CMAKE_PATH=C:/cmake
-set MINGW_PATH=C:/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0
-
-PATH=%CMAKE_PATH%/bin;%MINGW_PATH%/mingw64/bin;%MINGW_PATH%/mingw64/x86_64-w64-mingw32/include;%MINGW_PATH%/mingw64/x86_64-w64-mingw32/lib
-cmake -G "MinGW Makefiles"
