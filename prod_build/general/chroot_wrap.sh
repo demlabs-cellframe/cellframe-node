259b8c76b6daffc562af295824f90cb232533b95
alexandr.kravchenko <alexander.kravchenko@demlabs.net>
2020-04-27 17:54:33 +0500

    Added forced cellframe-python-sdk submodule update

--- prod_build/general/chroot_wrap.sh
+++ prod_build/general/chroot_wrap.sh
@@ -9,6 +9,12 @@ SRC_PATH=$4
 JOB=$5
 
 cd $SRC_PATH
+
+## Updating submodules for python sdk
+cd cellframe-sdk-python
+git submodule update --init
+cd ../
+## End of updating.
 echo "workdir is $(pwd)"
 . prod_build/general/pre-build.sh
 export_variables "./prod_build/general/conf/*"
