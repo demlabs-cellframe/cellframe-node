b3aa97d7cfd2d3fe6d99bfebd5e5b71d696a5d4e
roman.ovchinnikov <vn74@debian-4-test.vn74nest>
2020-07-21 02:45:11 -0400

    Couldnt move deb packages to /production/builds , messed with path. Also some output arrangement made in deploy.sh

--- prod_build/linux/debian/scripts/deploy.sh
+++ prod_build/linux/debian/scripts/deploy.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-echo "Deploying to $PACKAGE_PATH"
-echo $wd
+#assign path variables & show it
+
+cd packages
+wd=$(pwd)
+#echo "working directory is: $wd"
+cd ../../../..
+prod_dir=$(pwd)
+echo "production directory is: $prod_dir"
+echo -n "working directory is: " && cd -
+echo "Deploying to $prod_dir/$PACKAGE_PATH"
+echo
+
 
 CELLFRAME_REPO_CREDS="admin@debian.pub.demlabs.net"
 CELLFRAME_REPO_KEY="~/.ssh/demlabs_publish"
 CELLFRAME_REPO_PATH="~/web/debian.pub.demlabs.net/public_html"
 CELLFRAME_FILESERVER_CREDS="admin@pub.cellframe.net"
 CELLFRAME_FILESERVER_PATH="~/web/pub.cellframe.net/public_html/linux"
-pwd
 
-cd packages
+
 PKGFILES=$(ls . | grep .deb)
+echo -e "found:\n\e[32m$PKGFILES\e[0m\nin:" && pwd
+#pwd && echo $PKGFILES
 #cd ..
 
 #echo "We have $DISTR_CODENAME there"
@@ -21,7 +32,9 @@ for pkgfile in $PKGFILES; do
 	pkgname=$(echo $pkgfile | sed 's/.deb$//')
 	pkgname_public=$(echo $pkgname | cut -d '-' -f1-4,7-) #cutting away Debian-9.12
 	pkgname_weblink="$(echo $pkgname | cut -d '-' -f2,8 )-latest" #leaving only necessary entries
-	mv $pkgfile $wd/$PACKAGE_PATH/$pkgname$MOD.deb || { echo "[ERR] Something went wrong in publishing the package. Now aborting."; exit -4; }
+	mv $pkgfile $prod_dir/$PACKAGE_PATH/$pkgname$MOD.deb || { echo "[ERR] Something went wrong in publishing the package. Now aborting."; exit -4; } 
+	echo "Move package to production dir: "
+	echo -e "\e[32m$pkgfile moved to $prod_dir/$PACKAGE_PATH sucsessfully! \e[0m"
 	CODENAME=$(echo $pkgname | rev | cut -d '-' -f1 | rev)
 	cp -r ../prod_build/general/essentials/weblink-latest ../prod_build/general/essentials/$pkgname_weblink
 	sed -i "/document/s/cellframe.*deb/$pkgname_public$MOD.deb/" ../prod_build/general/essentials/$pkgname_weblink/index.php
