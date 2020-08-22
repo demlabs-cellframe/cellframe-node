7e39b6f947b91c6a2e44fd1f8eec6fcac20c9a45
alexander.kravchenko <alexander.kravchenko@demlabs.net>
2020-08-22 18:13:46 +0500

    likely fixed the condition

--- prod_build/linux/debian/scripts/deploy.sh
+++ prod_build/linux/debian/scripts/deploy.sh
@@ -28,7 +28,8 @@ for pkgfile in $PKGFILES; do
 	CODENAME=$(echo $pkgname | rev | cut -d '-' -f1 | rev)
 	cp -r ../prod_build/general/essentials/weblink-latest ../prod_build/general/essentials/$pkgname_weblink
 	sed -i "/document/s/cellframe.*deb/$pkgname_public$MOD.deb/" ../prod_build/general/essentials/$pkgname_weblink/index.php
-	if [[ $(echo $CI_COMMIT_REF_NAME | grep "master\|^release") ]]; then
+	if [[ $(echo $CI_COMMIT_REF_NAME | grep "master\|^release") != "" ]]; then
+		echo "REF_NAME is $CI_COMMIT_REF_NAME"
 		scp -i $CELLFRAME_REPO_KEY $wd/$PACKAGE_PATH/$pkgname$MOD.deb "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/$pkgname_public$MOD.deb"
 		scp -r -i $CELLFRAME_REPO_KEY ../prod_build/general/essentials/$pkgname_weblink "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/"
 		if [[ $CI_COMMIT_REF_NAME == "master" ]]; then
