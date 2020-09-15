098983b3dec94f100aa93552fc7acd95e120038e
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-09-15 16:17:03 +0700

    [*] Fixes

--- sources/main.c
+++ sources/main.c
@@ -265,7 +265,7 @@ int main( int argc, const char **argv )
         return -82;
     }
 
-    if ( dap_stream_ctl_init(32) != 0 ){
+    if ( dap_stream_ctl_init() != 0 ){
         log_it( L_CRITICAL, "Can't init stream control module" );
         return -83;
     }
