dfd642f91c4462c92639510ed95c1438f8a2afd3
Roman Khlopkov <roman.khlopkov@demlabs.net>
2020-10-09 04:32:23 +0000

    bugs-4547

--- sources/sig_unix_handler.c
+++ sources/sig_unix_handler.c
@@ -1,7 +1,9 @@
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "dap_common.h"
+#include "dap_events.h"
 #include "sig_unix_handler.h"
 
 #define LOG_TAG "sig_unix_handler"
@@ -16,17 +18,21 @@ static void clear_pid_file() {
         fclose(f);
 }
 
-_Noreturn static void sig_exit_handler(int sig_code) {
+static void sig_exit_handler(int sig_code) {
     log_it(L_DEBUG, "Got exit code: %d", sig_code);
     clear_pid_file();
+    //dap_events_stop_all();
     exit(0);
 }
 
 int sig_unix_handler_init(const char *pid_path) {
     l_pid_path = strdup(pid_path);
-    signal(SIGINT, sig_exit_handler);
-    signal(SIGTERM, sig_exit_handler);
-    signal(SIGHUP, sig_exit_handler);
+    struct sigaction new_action, old_action;
+    new_action.sa_handler = sig_exit_handler;
+    new_action.sa_flags = 0;
+    sigaction(SIGTERM, &new_action, &old_action);
+    sigaction(SIGINT, &new_action, &old_action);
+    sigaction(SIGHUP, &new_action, &old_action);
     return 0;
 }
 
