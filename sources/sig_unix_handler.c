fd44aa71ef4953de104e6da4e30a4e58892f6284
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2021-03-23 14:51:34 +0700

    [!] Dirty hack to workaroung bug with mkdir/getdir calls

--- sources/sig_unix_handler.c
+++ sources/sig_unix_handler.c
@@ -9,10 +9,10 @@
 
 #define LOG_TAG "sig_unix_handler"
 
-static const char *l_pid_path;
+static const char *s_pid_path = NULL;
 
 static void clear_pid_file() {
-    FILE * f = fopen(l_pid_path, "w");
+    FILE * f = fopen(s_pid_path, "w");
     if (f == NULL)
         log_it(L_WARNING, "Pid file not cleared");
     else
@@ -30,9 +30,11 @@ static void sig_exit_handler(int sig_code) {
 int sig_unix_handler_init(const char *a_pid_path) 
 {
     char * l_pid_dir = dap_path_get_dirname(a_pid_path);
+    sleep(200); // Don't know why but without it it crashes O_o 
     dap_mkdir_with_parents(l_pid_dir);
     DAP_DELETE(l_pid_dir);
-    l_pid_path = strdup(a_pid_path);
+
+    s_pid_path = strdup(a_pid_path);
     
     struct sigaction new_action, old_action;
     new_action.sa_handler = sig_exit_handler;
@@ -44,7 +46,8 @@ int sig_unix_handler_init(const char *a_pid_path)
 }
 
 int sig_unix_handler_deinit() {
-    free((char*)l_pid_path);
+    if( s_pid_path )
+	DAP_DELETE(s_pid_path);
     signal(SIGTERM, SIG_DFL);
     signal(SIGINT, SIG_DFL);
     signal(SIGHUP, SIG_DFL);
