befe0651ade86a92f80fcc17c9abe3221fbb193c
denis.sumin <denis.sumin@demlabs.net>
2023-05-03 17:03:42 +0300

    [+] added cli data

--- diagtool/LinuxDiagnostic.h
+++ diagtool/LinuxDiagnostic.h
@@ -34,16 +34,29 @@ private:
     QJsonObject get_sys_info();
     bool get_cpu_times(size_t &idle_time, size_t &total_time);
     std::vector<size_t> get_cpu_times();
+    long get_pid();
     QString get_running(char* pid);
     QString get_proc_path(long pid);
     QJsonObject get_process_info(long pid, int totalRam);
+    QJsonObject get_disk_info();
+    QJsonObject get_cli_info();
+
+
+    //CLI
+
+    QStringList get_networks();
+    QJsonObject get_net_info(QString net);
+    QJsonObject get_mempool_count(QString net);
+    QJsonObject get_ledger_count(QString net);
+    QJsonObject get_blocks_count(QString net);
+    QJsonObject get_events_count(QString net);
 
 private slots:
     void info_update();
 
 private:
     size_t previous_idle_time{0}, previous_total_time{0};
-    long get_pid();
+    bool s_node_status{false};
 
 };
 
