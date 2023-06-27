f2e355667c10c3fae1363954a084120019fa940d
Constantin P <papizh.konstantin@demlabs.net>
2023-06-27 06:29:10 +0000

    [+] diagtool fpe fix.

--- sources/cellframe_node.h
+++ sources/cellframe_node.h
@@ -1,12 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#ifdef __ANDROID__
+#include <jni.h>
+#endif
+
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-int cellframe_node_Main(int argc, const char **argv);
-int cellframe_node__cli_Main(int argc, const char *argv[]);
+JNIEXPORT int Java_com_CellframeWallet_Node_cellframeNodeMain(JNIEnv *javaEnv, jobject __unused jobj, jobjectArray argv);
+JNIEXPORT int Java_com_CellframeWallet_Node_dapCommonInit(JNIEnv *javaEnv, jobject __unused jobj, jobjectArray argv);
+JNIEXPORT jstring Java_com_CellframeWallet_Node_cellframeNodeCliMain(JNIEnv *javaEnv, jobject __unused jobj, jobjectArray argv);
+
+int cellframe_node_cli_Main(int argc, const char *argv[], const char *sys_dir, char **l_out_res);
 int cellframe_node_tool_Main(int argc, const char **argv);
 
 #ifdef __cplusplus
