3aea5edbbc812115d6789b237d212563139e649d
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-03-15 06:42:55 +0300

    [*] fix

--- sources/main_node_tool.c
+++ sources/main_node_tool.c
@@ -6,9 +6,9 @@
  * Copyright  (c) 2017-2019
  * All rights reserved.
 
- This file is part of DAP (Deus Applications Prototypes) the open source project
+ This file is part of DAP (Demlabs Application Protocol) the open source project
 
-    DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+    DAP (Demlabs Application Protocol) is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
