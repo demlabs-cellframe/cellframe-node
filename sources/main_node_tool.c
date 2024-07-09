4c759dd15af8299e9700c661a1c60718c6c6bb70
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-07-09 16:33:27 +0700

    [+] I added a line with a list of supported signature types for wallets and added the output of this line to the utility and to the wallet new command.

--- sources/main_node_tool.c
+++ sources/main_node_tool.c
@@ -160,7 +160,7 @@ static int s_wallet_create(int argc, const char **argv) {
     }
 
     if ( l_sig_type.type == SIG_TYPE_NULL ) {
-      log_it( L_ERROR, "Wrong signature '%s'", argv[4] );
+      log_it( L_ERROR, "Invalid signature type '%s', you can use the following:\n%s", argv[4], g_str_valid_type_sign_for_wallet);
       s_help( );
       exit( -2004 );
     }
@@ -170,7 +170,7 @@ static int s_wallet_create(int argc, const char **argv) {
     //
     if (l_sig_type.type == SIG_TYPE_TESLA)
     {
-        log_it( L_ERROR, "Tesla algorithm is not supported, please, use another variant");
+        log_it( L_ERROR, "Tesla algorithm is not supported, please, use another variant:\n%s", g_str_valid_type_sign_for_wallet);
         exit( -2004 );
     }
 
