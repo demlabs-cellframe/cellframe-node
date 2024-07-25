8bbabffc7a315eba4821625c7ad01a04160fe97d
dmitry <dmitry.puzyrkov@demlabs.net>
2024-07-25 14:32:03 +0700

    [*] conftool macos: use osascript for priveleged ops.

--- conftool/service/macos_auth.m
+++ conftool/service/macos_auth.m
@@ -3,6 +3,9 @@
 
 #import <Foundation/Foundation.h>
 
+#include "Foundation/NSString.h"
+#include "Foundation/NSAppleScript.h"
+#
 
 int callSec (char *tool, char* args[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
@@ -26,6 +29,21 @@ int callSec (char *tool, char* args[]) {
 	
     [pool drain];
     return -1;
+}
+
+
+int callSecScript(char *script)
+{
+    NSDictionary *error = [NSDictionary new];
+    NSString *appleScriptString = @"do shell script ";
+    appleScriptString = [appleScriptString stringByAppendingString:@"\""];
+    appleScriptString = [appleScriptString stringByAppendingString:[NSString stringWithUTF8String: script]];
+    appleScriptString = [appleScriptString stringByAppendingString:@"\""];
+    appleScriptString = [appleScriptString stringByAppendingString:@" with administrator privileges"];
+    NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:appleScriptString];
+    NSString *result;
+    result = [[appleScript executeAndReturnError: &error] stringValue];
+    return 0;
 }
 
 #endif
