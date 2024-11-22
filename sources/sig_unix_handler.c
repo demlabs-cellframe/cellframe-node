aca48d3280169c7eb184b562bdad4449d6ff3e99
Constantin P. <papizh.konstantin@demlabs.net>
2024-11-22 09:18:55 +0000

    Hotfix exit tmp

--- sources/sig_unix_handler.c
+++ sources/sig_unix_handler.c
@@ -38,9 +38,10 @@ static void clear_pid_file() {
 }
 
 static void sig_exit_handler(int sig_code) {
+    
     log_it(L_DEBUG, "Got exit code: %d", sig_code);
-
-    clear_pid_file();
+    exit(sig_code);
+    /*clear_pid_file();
 	
     dap_plugin_deinit();
     dap_chain_node_mempool_autoproc_deinit();
@@ -64,7 +65,7 @@ static void sig_exit_handler(int sig_code) {
     log_it(L_NOTICE,"Stopped Cellframe Node");
     fflush(stdout);
 
-    exit(0);
+    exit(0);*/
 }
 
 
