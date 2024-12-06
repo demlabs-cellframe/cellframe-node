4e8767ec98a61ff14d1b9da63b933124c09f7c49
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-12-06 15:44:57 +0700

    [*] Getting a list of plugins, by directories and deleting files has been moved to separate functions. Parsing of action parameters has also been redesigned.

--- conftool/commands/PluginsCommand.cpp
+++ conftool/commands/PluginsCommand.cpp
@@ -13,54 +13,81 @@ CPluginsCommand::CPluginsCommand(std::vector <std::string> a_cmd_tokens) : CAbst
     if (a_cmd_tokens.size() < 2)
         throw std::invalid_argument("plugins command require action argument");
 
-    std::string l_command = a_cmd_tokens[1];
-    if (l_command == "install") {
-        this->action = CPluginsActions::INSTALL;
-    } else if (l_command == "ensure") {
-        this->action = CPluginsActions::ENSURE;
-    } else if (l_command == "update") {
-        this->action = CPluginsActions::UPDATE;
-    } else if (l_command == "remove") {
-        this->action = CPluginsActions::REMOVE;
-    } else if (l_command == "list") {
-        this->action = CPluginsActions::LIST;
-    } else {
-        this->action = CPluginsActions::UNDEFINED;
+    this->action = a_cmd_tokens[1];
+    //Exchange Params
+    for (int i = 2; i < a_cmd_tokens.size(); i++) {
+        this->params.push_back(a_cmd_tokens[i]);
     }
-//    if (CellframeConfigurationFile::exists("",""))
-//    this->
+//    std::iterator<std::string> iter = a_cmd_tokens.begin(), a_cmd_tokens.end()
+
+//    this->params = std::rotate_copy(a_cmd_tokens.begin(), a_cmd_tokens.end(),)
+
 }
 
 bool CPluginsCommand::execute(bool non_interactive, int flags) {
+    this->flags = flags;
     CellframeConfigurationFile cfg(fs::path{variable_storage["CONFIGS_PATH"]}/"etc"/"cellframe-node.cfg", flags);
 
     std::string l_path_plugins;
     if (!cfg.exists("plugins", "py_path", &l_path_plugins))
         throw std::invalid_argument("The configuration file does not have a plugins section or it does not have a py_path value.");
+    this->pathPlugin = l_path_plugins;
     if (flags & F_VERBOSE) std::cout << "[VC] Path to the directory with plugins '" << l_path_plugins << "'" << std::endl;
-    switch (this->action) {
-        case LIST: {
-            if (!fs::exists(l_path_plugins)) {
-                std::cout << "The directory with plugins is missing, try to activate work with plugins again."
-                          << std::endl;
+    if (this->action == "install") {
+    } else if (this->action == "ensure") {
+    } else if (this->action == "update") {
+    } else if (this->action == "remove") {
+        if (this->params.size() == 0)
+            throw  std::invalid_argument("Not enough arguments to execute the command to remove the plugin.");
+        return actionRemovePlugin(params[0]);
+    } else if (this->action == "list") {
+        std::vector<std::string> listPlugins = getListPlugins(l_path_plugins);
+        std::cout << "List plugins (" << listPlugins.size() <<"):" << std::endl;
+        for (auto &entry : listPlugins){
+            std::cout << "\t" << entry << "\n";
+        }
+        return true;
+    } else {
+        throw std::invalid_argument("Undefined action for command plugins");
+    }
+    return false;
+}
+
+
+std::vector<std::string> CPluginsCommand::getListPlugins() {
+    return getListPlugins(this->pathPlugin);
+}
+
+std::vector<std::string> CPluginsCommand::getListPlugins(std::filesystem::path path) {
+    std::vector<std::string> ret;
+    fs::directory_iterator l_plugins_dir(path);
+    for (const fs::directory_entry &entry : l_plugins_dir) {
+        if (entry.is_directory()) {
+            fs::path l_plugin_path = entry.path();
+            fs::path l_manifest_file_path = fs::path{l_plugin_path}/"manifest.json";
+            if (fs::exists(l_manifest_file_path)) {
+                ret.push_back(entry.path().filename().generic_string());
+            }
+        }
+    }
+    return ret;
+}
+
+bool CPluginsCommand::actionRemovePlugin(std::string a_name_plugin) {
+    std::vector<std::string> listPlugins = getListPlugins();
+    for (std::string l_name_plugin : listPlugins) {
+        if (l_name_plugin == a_name_plugin) {
+            std::error_code err;
+            const std::uintmax_t n(fs::remove_all(this->pathPlugin/a_name_plugin, err));
+            if (!err){
+                std::cout << "Plugin '" << a_name_plugin << "' plugin removed. Removed: " << n << " elements." << std::endl;
+                return true;
+            } else {
+                std::cout << "Can't remove '" << a_name_plugin << "' plugin. Error: " << err.message() << std::endl;
                 return false;
             }
-            std::cout << "List plugins: " << std::endl;
-            fs::directory_iterator l_plugins_dir(l_path_plugins);
-            for (const fs::directory_entry &entry : l_plugins_dir) {
-                if (entry.is_directory()) {
-                    fs::path l_plugin_path = entry.path();
-                    fs::path l_manifest_file_path = fs::path{l_plugin_path}/"manifest.json";
-                    if (!fs::exists(l_manifest_file_path)) {
-                        std::cout << "In directory plugin '" << l_plugin_path << "' absent manifest plugin" << std::endl;
-                    }
-                    std::cout << "\t " << entry.path().filename().generic_string() << std::endl;
-                }
-            }
-        } break;
-        default: {
-            throw std::invalid_argument("Undefined action for command plugins");
-        }break;
+        }
     }
+    std::cout << "Can't find '" << a_name_plugin << "' plugin." << std::endl;
     return false;
 }
--- conftool/commands/PluginsCommand.h
+++ conftool/commands/PluginsCommand.h
@@ -1,34 +1,16 @@
 #include "AbstractCommand.h"
 
-enum CPluginsActions{
-        ENSURE,
-        INSTALL,
-        UPDATE,
-        LIST,
-        REMOVE,
-        UNDEFINED
-};
-
-
 class CPluginsCommand : public CAbstractScriptCommand{
 public:
     CPluginsCommand(std::vector <std::string> a_cmd_tokens);
     static std::unique_ptr<CAbstractScriptCommand> create(std::vector <std::string> cmd_tokens) { return std::make_unique<CPluginsCommand>(cmd_tokens); }
     bool execute(bool non_interactive, int flags);
 private:
-    CPluginsActions action;
-    union {
-        struct {
-            char *source_path;
-            bool enabled_install_dependencies;
-            bool l2;
-        } install;
-        struct {
-            bool binary;
-        } ensure;
-        struct {} update;
-        struct {} remove;
-    };
-    std::string python_plugin_path;
-    std::string binary_plugin_path;
+    std::string action;
+    std::vector<std::string> params;
+    int flags;
+    std::filesystem::path pathPlugin;
+    std::vector<std::string> getListPlugins();
+    std::vector<std::string> getListPlugins(std::filesystem::path path);
+    bool actionRemovePlugin(std::string name_plugin);
 };
