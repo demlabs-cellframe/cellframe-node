5f3c866dbc685ea666013888a2734787ff6fac50
Olzhas <oljas.jarasbaev@demlabs.net>
2024-12-16 12:38:49 +0700

    [*] port-13903

--- sources/cellframe-node.c
+++ sources/cellframe-node.c
@@ -374,9 +374,6 @@ int main( int argc, const char **argv )
         return -66;
     }
 
-    if( dap_chain_net_srv_order_init() )
-        return -67;
-
     if (dap_chain_net_srv_xchange_init()) {
         log_it(L_ERROR, "Can't provide exchange capability");
     }
@@ -426,6 +423,19 @@ int main( int argc, const char **argv )
     if( dap_chain_wallet_cache_init() ) {
         log_it(L_CRITICAL,"Can't init dap chain wallet module");
         return -61;
+    }
+
+    if( dap_chain_net_srv_order_init() )
+        return -67;
+
+    if (dap_chain_node_list_clean_init()) {
+        log_it( L_CRITICAL, "Can't init node list clean" );
+        return -131;
+    }
+
+    if (dap_global_db_clean_init()) {
+        log_it( L_CRITICAL, "Can't init gdb clean and pin" );
+        return -133;
     }
 
     log_it(L_INFO, "Automatic mempool processing %s",
